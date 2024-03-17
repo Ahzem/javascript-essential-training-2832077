ec684f18077a0c4ac2636fa4a263f8083f0e9b5d
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-03-17 13:46:33 +0530

    Practice Completed

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,31 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+class Clothing {
+  constructor(type, color, size, price, brand) {
+    this.type = type;
+    this.color = color;
+    this.size = size;
+    this.price = price;
+    this.brand = brand;
+  }
+
+  changePrice(newPrice) {
+    this.price = newPrice;
+  }
+}
+
+const shirt = new Clothing("shirt", "blue", "M", 20, "Nike");
+const pants = new Clothing("pants", "black", "L", 30, "Adidas");
+const socks = new Clothing("socks", "white", "M", 5, "Hanes");
+
+console.log("Before: ", shirt.price);
+shirt.changePrice(25);
+console.log("After: ", shirt.price);
+console.log("Before: ", pants.price);
+pants.changePrice(35);
+console.log("After: ", pants.price);
+console.log("Before: ", socks.price);
+socks.changePrice(10);
+console.log("After: ", socks.price);
